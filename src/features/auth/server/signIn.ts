e1cf920067c8bc4c0fb6c08582584778faa51301
matchaejayyy <145851990+matchaejayyy@users.noreply.github.com>
2026-05-25 21:17:45 +0800

    updated again

--- src/features/auth/server/signIn.ts
+++ src/features/auth/server/signIn.ts
@@ -1,16 +1,20 @@
 import { createServerFn } from "@tanstack/react-start";
+import { getRequestHeaders } from "@tanstack/react-start/server";
 import { auth } from "@/integrations/better-auth/auth";
 import { SignInSchema } from "../schemas/auth";
 
 const signIn = createServerFn({ method: "POST" })
 	.inputValidator(SignInSchema)
 	.handler(async ({ data }) => {
+		const headers = getRequestHeaders();
+
 		if (data.method === "email") {
 			const res = await auth.api.signInEmail({
 				body: {
 					email: data.email,
 					password: data.password,
 				},
+				headers,
 			});
 
 			return res.user;
@@ -20,6 +24,7 @@ const signIn = createServerFn({ method: "POST" })
 					username: data.username,
 					password: data.password,
 				},
+				headers,
 			});
 
 			return res.user;
